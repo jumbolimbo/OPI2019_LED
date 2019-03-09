232e2f3163a9b481d9fdabdfb9546650ea2afa49
jumbolimbo <thoffmann706@gmail.com>
2019-03-09 01:54:51 -0800

    Look at this

--- two_led/two_led.ino
+++ two_led/two_led.ino
@@ -3,16 +3,23 @@
 #define     FHT_N               256         // Number of FFT bins
 #include <FHT.h> 
 #define COLOR_ORDER RGB
-#define BRIGHTNESS 250
+#define BRIGHTNESS 200
 #define STRIPS 2
-#define LEDS_PER_STRIP 150
+#define LEDS_PER_STRIP 450
 CRGB  leds[STRIPS][LEDS_PER_STRIP];
 int topAverages[50];
 int topVariances[50];
 int topZScores[50];
-float bassScore;
-float midScore;
-float highScore;
+float volumeScore;
+float bassScore = 40;
+float midScore = 36;
+float highScore = 32;
+float budget = 128;
+
+float volumeScoreMean = 40 + 36 + 32;
+float bassScoreMean = 40;
+float midScoreMean = 36;
+float highScoreMean = 32;
 
 int sensorPin = A0;    // select the input pin for the potentiometer
 float mean = 400;
@@ -20,13 +27,13 @@ float variance = 30;
 int reading = 0;
 int zScore;
 
+extern const TProgmemPalette16 capAmerica_p PROGMEM;
+extern CRGBPalette16 hulk_p;
 
 void setup() {
-  int data[128];
-//  index = data;
   delay(1000);
-  FastLED.addLeds<WS2812, 8, GRB>(leds[0], LEDS_PER_STRIP).setCorrection(TypicalLEDStrip);
-  FastLED.addLeds<WS2812, 9, GRB>(leds[1], LEDS_PER_STRIP).setCorrection(TypicalLEDStrip);
+  FastLED.addLeds<WS2812B, 8, GRB>(leds[0], LEDS_PER_STRIP).setCorrection(TypicalLEDStrip);
+  FastLED.addLeds<WS2812B, 9, GRB>(leds[1], LEDS_PER_STRIP).setCorrection(TypicalLEDStrip);
   Serial.begin(9600);
   
   TIMSK0 = 0; // turn off timer0 for lower jitter
@@ -43,52 +50,81 @@ void loop() {
   fht_lin_out[1] = 0;
   fht_lin_out[2] = 0;
   computeRanges();
-  
-  for (uint8_t px = 0; px < 50; px++) {
-     setLED(px, bassScore, midScore, highScore);
+  computeRangeMoving();
+  Serial.print(bassScoreMean);
+  Serial.print(" , ");
+  Serial.print(midScoreMean);
+  Serial.print(" , ");
+  Serial.println(highScoreMean);
+  bassScore = budget*(bassScore/volumeScore);
+  midScore = budget*(midScore/volumeScore);
+  highScore = budget*(highScore/volumeScore);
+
+  if(budget < 16 && volumeScoreMean > 0){
+    volumeScoreMean = volumeScoreMean*0.99+0.01;
+  } else if (budget > 512){
+    volumeScoreMean = volumeScoreMean*1.01 - 0.01;
   }
+   static uint8_t startIndex = 0;
+    startIndex = startIndex + 1;
   
   
-  Serial.println(fht_lin_out[2]);
+  wholeStripEffect(0,250);
+  //FillLEDsFromPaletteColors(0,450, startIndex, capAmerica_p, NOBLEND);
+  
+  
   FastLED.show();
-  //Fire(55,120,15);
-//  for(int i = 0; i < LEDS_PER_STRIP; i = i + 1){
-//    doFHT();
-//    setLED(leds[0], i, 255, 0, 0);
-//    FastLED[0].showLeds(BRIGHTNESS);
-//    FastLED[1].showLeds(BRIGHTNESS);
-//    //delay(1);
-//  }
-//  for(int i = 0; i < LEDS_PER_STRIP; i = i + 1){
-//    doFHT();
-//    setLED(leds[0], i, 0, 0, 0);
-//    FastLED[0].showLeds(BRIGHTNESS);
-//    FastLED[1].showLeds(BRIGHTNESS);
-//    //delay(1);
-//  }
-  delay(20);
+  delay(10);
+}
+inline void wholeStripEffect(int low, int high){
+  for (uint8_t px = low; px < high; px+= 2) {
+     setLED(px, max(min(bassScore,255),0), max(min(midScore,255),0), max(min(highScore,255),0));
+  }
+}
+
+
+void FillLEDsFromPaletteColors(int low, int high, uint8_t colorIndex, CRGBPalette16 palette, TBlendType blending)
+{
+    uint8_t brightness = 255;
+    
+    for( int i = low; i < high; i++) {
+        leds[0][i] = ColorFromPalette( palette, colorIndex, brightness, blending);
+        colorIndex += 1;
+    }
 }
 
 inline void computeRanges(){
   bassScore = 0;
-  for(int i = 0; i < 5; i++){
+  for(int i = 5; i < 10; i++){
     bassScore += topAverages[i];
   }
-  bassScore = bassScore / 5;
+  bassScore = bassScore / 10;
   
   midScore = 0;
-  for( int i = 5; i < 25; i++){
+  for( int i = 10; i < 20; i++){
     midScore += topAverages[i];
   }
-  midScore = midScore / 20;
+  midScore = midScore / 10;
   
   highScore = 0;
   
-  for(int i = 25; i < 50; i++){
+  for(int i = 20; i < 50; i++){
     highScore += topAverages[i];
   }
-  highScore = highScore / 20;
+  highScore = highScore / 10;
+
+  volumeScore = highScore + midScore + bassScore;
+  
 }
+
+inline void computeRangeMoving(){
+  budget = 0.5 * 128 + 6*(volumeScore - volumeScoreMean) + 0.5 * budget;
+  bassScoreMean = bassScoreMean*0.9999 + bassScore*0.0001;
+  midScoreMean = midScoreMean*0.9999 + midScore*0.0001;
+  highScoreMean = highScoreMean*0.9999 + highScore*0.0001;
+  volumeScoreMean = volumeScoreMean*0.99 + volumeScore*0.01;
+}
+
 
 inline void readAnalog(int updates){
   for (int i = 0 ; i < updates ; i++) { 
@@ -132,10 +168,11 @@ inline void computeFFTMoving(){
 inline void computeMoving(){
   readAnalog(1);
   reading = fht_input[0];
-  mean = mean*0.9 + reading*0.1;
-  variance = variance * 0.9 + abs(reading - mean)*0.1;
+  mean = mean*0.9999 + reading*0.0001;
+  variance = variance * 0.9999 + abs(reading - mean)*0.0001;
   zScore = abs( (reading - mean)/variance);
 }
+
 
 inline void setLED(int led, uint8_t r, uint8_t g, uint8_t b){
   leds[0][led].r = r;
@@ -152,7 +189,7 @@ inline void setLED(CRGB strip[], int led, uint8_t r, uint8_t g, uint8_t b){
   strip[led].b = b;
 }
 
-const TProgmemPalette16 myRedWhiteBluePalette_p PROGMEM =
+const TProgmemPalette16 capAmerica_p PROGMEM =
 {
     CRGB::Red,
     CRGB::Gray, // 'white' is too bright compared to red and blue
@@ -170,6 +207,16 @@ const TProgmemPalette16 myRedWhiteBluePalette_p PROGMEM =
     CRGB::Gray,
     CRGB::Blue,
     CRGB::Blue,
-    CRGB::Gray,
-    CRGB::Gray
+    CRGB::Black,
+    CRGB::Black
 };
+
+CRGB purple = CHSV( HUE_PURPLE, 255, 255);
+    CRGB green  = CHSV( HUE_GREEN, 255, 255);
+    CRGB black  = CRGB::Black;
+    
+CRGBPalette16 hulk_p = CRGBPalette16(
+                                   green,  green,  black,  black,
+                                   purple, purple, black,  black,
+                                   green,  green,  black,  black,
+                                   purple, purple, black,  black );
